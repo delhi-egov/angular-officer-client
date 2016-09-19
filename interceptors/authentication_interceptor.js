f8e255d7813c7f3aa05380e6a8e771dc25694796
Vaibhav Sinha <vaibhavsinh@gmail.com>
2016-09-19 11:53:38 +0530

    Fix interceptor

--- interceptors/authentication_interceptor.js
+++ interceptors/authentication_interceptor.js
@@ -1,13 +1,15 @@
-module.exports = function($q, $location) {
+module.exports = function($q, $location, $injector) {
     return {
-            response: function(response){
+            response: function(response) {
                 return response || $q.when(response);
             },
             responseError: function(rejection) {
-                if (rejection.status === 401) {
-                    $location.path('/login');
-                }
+	        if (rejection.status === 401) {
+	            $injector.get('$state').go('user.login');
+	        }
                 return $q.reject(rejection);
             }
         };
 };
+
+//http://stackoverflow.com/questions/20230691/injecting-state-ui-router-into-http-interceptor-causes-circular-dependency
