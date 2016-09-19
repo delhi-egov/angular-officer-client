6a29aa96520836152ccc305dad8dc915f3772392
Vaibhav Sinha <vaibhavsinh@gmail.com>
2016-09-19 12:14:25 +0530

    Returning promise

--- services/application_service.js
+++ services/application_service.js
@@ -1,19 +1,23 @@
 module.exports = function($state, backendClient, authInfo, applicationInfo) {
 	return {
 		getHistoricalProcess: function(applicationId) {
-			backendClient.getHistoricalApplication(applicationId)
-			.then(function(response) {
-					if(response.data) {
-                    	applicationInfo.application = response.data[0];
-                    	resolve(response.data[0]);
-                	}
-                	else {
-                		reject("No application with given application id found");
-                	}
-                }, function(response) {
-                    controller.getApplicationError = response.message;
-                    reject(response.message);
-                });
+			var promise = new Promise(function(resolve, reject) {
+				backendClient.getHistoricalApplication(applicationId)
+				.then(function(response) {
+					if(response.data.data) {
+						applicationInfo.application = response.data.data[0];
+						resolve(response.data.data[0]);
+					}
+					else {
+						controller.getApplicationError = "No application with given application id found";
+						reject("No application with given application id found");
+					}
+				}, function(response) {
+					controller.getApplicationError = response.message;
+					reject(response.message);
+				});
+			});
+			return promise;
 		}
 	}
 }
